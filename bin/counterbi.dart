7a00d3a30f022e8cafd7b8933f11ec1f98ca84fc
jhon <Jhonmilerrons@outlook.eso>
2022-02-08 22:21:06 -0500

    tercer commit

--- bin/counterbi.dart
+++ bin/counterbi.dart
@@ -1,13 +1,10 @@
-
-
 import 'package:examples_dart/exampless/counter.dart';
 
-main()async{
- 
- final s = Counter();
- s.increment();
- 
- print(s.entero);
- var ss = await s.getData();
- print(ss);
+main() async {
+  final s = Counter();
+  s.increment();
+
+  print(s.entero);
+  var ss = await s.getData();
+  print(ss);
 }
--- test/examples_test/counter_test.dart
+++ test/examples_test/counter_test.dart
@@ -1,29 +1,31 @@
-
-
 import 'package:examples_dart/exampless/counter.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-void main(){
+void main() {
+  group('counter Class', () {
+    test('counter value entero increment 1', () {
+      var k = Counter();
+      k.entero;
+      k.increment();
+      print(k.entero);
+      expect(k.entero, 1);
+    });
 
-  group('counter', () {
+    test('counter value entero decrement -1', () {
+      var k = Counter();
+      k.entero;
+      k.decremnte();
+      print(k.entero);
+      expect(k.entero, -1);
+    });
 
-   test('counter value entero increment 1', () {
-     
-    var k = Counter();
-  k.entero;
-  k.increment();
-  print(k.entero);
-  expect(k.entero, 1);
-   } ); 
+    test('should retorn DATA2 when incremented', () async {
+      var k = Counter();
+      k.increment();
+      var s = await k.getData();
 
-   test('counter value entero decrement -1', () {
-     
-    var k = Counter();
-  k.entero;
-  k.decremnte();
-  print(k.entero);
-  expect(k.entero, -1);
-   } ); 
-  } );
+      expect(s, 'Data2');
+    });
+  });
 }
