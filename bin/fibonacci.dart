984df524cd99c0b60b58b8c97110b0b74169b7f4
jhon <Jhonmilerrons@outlook.eso>
2022-04-08 14:48:16 -0500

    rama

--- bin/fibonacci.dart
+++ bin/fibonacci.dart
@@ -1,50 +1,38 @@
-void main(){
+void main() {
   List<int> nuemros1 = [9, 4, 3, 39, 12, 1, 4, 6];
 
   List<int> nuemros = [9, 1, 13, 6, 7, 5, 3, 4, 2];
-print(twosum(nuemros, 10));
-
+  print(twosum(nuemros, 20));
 }
- List<int>? twosum( List<int> array, target){
+
+List<int>? twosum(List<int> array, int target) {
   Map<dynamic, dynamic> diferencias = {};
-      //diferencias[target -n] = 1;
+  //diferencias[target -n] = 1;
 
-   for(var i = 0; i < array.length; i++){
-     final n = array[i];
+  for (var i = 0; i < array.length; i++) {
+    final n = array[i];
     // print(diferencias.containsValue([target-n]));
-     //if(diferencias.containsKey([target-n]) ){
-     // return [diferencias[target-1 ].index,i ]; 
-   // return [diferencias[target-1 ],4 ];
-      
-    int complement = target -array[1];
-    if(diferencias.containsKey(target-n) ){
-   print(diferencias);
-      return [diferencias[target-n] , i ];
-    }
-   print('diferencias');
- 
-   diferencias[n] =i; 
-     
-   
-   }
-  return [];
+    //if(diferencias.containsKey([target-n]) ){
+    // return [diferencias[target-1 ].index,i ];
+    // return [diferencias[target-1 ],4 ];
+    print('existe ${target - n}');
 
+    if (diferencias.containsKey(target - n)) {
+      print(
+          'El mapa dentro del If $diferencias  ||| el valor del  índice :$i || target :$target ');
+      return [diferencias[target - n], i];
+    }
+
+    diferencias[n] = i;
+    print('diferencias mapa$diferencias');
+  }
+  return [];
 }
 
-
-
-
-
-
-
-
-
-fibonacci(int i){
-
-  if(i <= 1) {
+fibonacci(int i) {
+  if (i <= 1) {
     return i;
   } else {
-    return fibonacci(i-1)+fibonacci(i-2);
+    return fibonacci(i - 1) + fibonacci(i - 2);
   }
 }
-
